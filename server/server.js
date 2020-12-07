1bc972486a26f8603574af036b2072a6060d8e03
HechengLi <11177048+HechengLi@users.noreply.github.com>
2020-12-06 23:40:51 -0500

    added token expire

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
+require("dotenv").config()
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const dayjs = require('dayjs')
+
 const app = new Koa()
 const router = new Router()
+
+const secret = process.env.JWT_SECRET || 'secret'
+const saltRounds = 10
 
 router
   .get('/', (ctx, next) => {
@@ -17,10 +25,28 @@ router
   .post('/default_error', (ctx, next) => {
     throw new Error()
   })
+  .post('/auth', async (ctx, next) => {
+    const { username, password } = ctx.request.body
+    if (!username) ctx.throw(422, 'Username required')
+    if (!password) ctx.throw(422, 'Password required')
+    const match = await bcrypt.compare(password, await bcrypt.hash('password', saltRounds))
+    if (
+      username !== 'username'
+      || !match
+    ) {
+      ctx.throw(401, 'Incorrect username/password')
+    }
+    const payload = { username, expire: dayjs().unix() }
+    const token = jwt.sign(payload, secret)
+    ctx.body = token
+    ctx.cookies.set("access_token", token, { httpOnly: true })
+  })
+  .get('/data', async (ctx, next) => {
+    ctx.body = ctx.request.jwtPayload.username
+  })
 
 app
   .use(bodyParser())
-
   .use(async (ctx, next) => {
     try {
       await next()
@@ -28,6 +54,28 @@ app
       //TODO: log error
       throw err
     }
+  })
+  .use(async (ctx, next) => {
+    if (ctx.request.url !== '/auth') {
+      if (!ctx.headers.authorization) ctx.throw(403, 'No Authorization header found')
+      const token = ctx.headers.authorization
+      try {
+        ctx.request.jwtPayload = jwt.verify(token.replace('Bearer ', ''), secret)
+      } catch (err) {
+        ctx.throw(401, 'Failed to verify token')
+      }
+    }
+    await next()
+  })
+  .use(async(ctx, next) => {
+    if (ctx.request.url !== '/auth') {
+      if (ctx.request.jwtPayload.expire < dayjs().unix()) { // token expired
+        const payload = { username: ctx.request.jwtPayload.username, expire: dayjs().unix() }
+        const token = jwt.sign(payload, secret)
+        ctx.cookies.set("access_token", token, { httpOnly: true })
+      }
+    }
+    await next()
   })
   .use(router.routes())
   .use(router.allowedMethods())
